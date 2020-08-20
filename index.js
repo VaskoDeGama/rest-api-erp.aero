675c22afe796de48def5c95b4ea84720d450a86d
VaskoDeGama <vasr161095@gmail.com>
2020-08-20 14:57:25 +0800

    add fileUploader and refactor database init

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const config = require('config')
-const express = require('express')
-const morgan = require('morgan')
-const Sequelize = require('sequelize')
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
-const DB_CONFIG = config.get('DB_CONFIG')
-const sequelize = new Sequelize({
-  ...DB_CONFIG,
-  logging: false,
-})
+const path = require("path");
 
+const database = require("./utils/database");
 
-const server = express()
-const PORT = process.env.PORT || 3000
+const server = express();
+const PORT = process.env.PORT || 3000;
 
-server.use(express.json())
-server.use(morgan(`dev`))
+server.use(logger("dev"));
+server.use(cors());
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "public")));
+server.use(express.urlencoded({ extended: false }));
 
 server.use((req, res) => {
   res.status(200).json({
-    status: 'I am alive',
-  })
-})
+    status: "I am alive",
+  });
+});
 
 function start() {
   try {
-
-    sequelize.authenticate()
-        .then(console.log('Connection has been established successfully.'))
-        .catch(e => console.error('Unable to connect to the database:', e))
-
+    database
+      .authenticate()
+      .then(console.log("Database has been connected successful."))
+      .catch((e) => console.error("Unable to connect to the database:", e));
     server.listen(PORT, () => {
-      console.log(`App started on ${PORT} port`)
-    })
+      console.log(`App started on port: ${PORT}.`);
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
-start()
+start();
